110cc5ac81331792c77f790390d9f4fef9b8155e
karcag.tamas <karcagtamas@gmail.com>
2019-03-01 22:56:56 +0100

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,8 +18,8 @@ import {
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { GeneratorComponent } from './generator/generator.component';
@@ -28,6 +28,7 @@ import { PortaComponent } from './porta/porta.component';
 import { NavigatorComponent } from './navigator/navigator.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 import { environment } from 'src/environments/environment';
 
 @NgModule({
